e3817ab79dce3ba7163aa3b27aeb268fc641d7ad
AllanDevincolagit <profissional.allandevincola@gmail.com>
2022-05-12 15:23:24 -0300

    Update de link nos cards, merge (mesclagem) feita entre a master e a teste, bug do iss express corrigido.

---
+++ InformeTqERegiao/Controllers/CardsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformeTqERegiao.Controllers
+{
+    public class CardsController : Controller
+    {
+        public IActionResult Farmacias()
+        {
+            return View();
+        }
+
+        public IActionResult Hospitais()
+        {
+            return View();
+        }
+
+        public IActionResult Supermercados()
+        {
+            return View();
+        }
+    }
+}

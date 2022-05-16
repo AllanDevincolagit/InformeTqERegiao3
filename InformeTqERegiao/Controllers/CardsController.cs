2b092aa534f34698cb4271e46ca9cfbfc2e59026
MaiiconDouglasd <maiicondouglasd@hotmail.com>
2022-05-16 00:21:41 -0300

    Organizando as pastas

---
+++ InformeTqERegiao/Controllers/CardsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InformeTqERegiao.Controllers
+{
+    public class CardsController : Controller
+    {
+        public IActionResult Saude()
+        {
+            return View();
+        }
+
+        public IActionResult Compras()
+        {
+            return View();
+        }
+
+        public IActionResult Lazer()
+        {
+            return View();
+        }
+
+        public IActionResult Variedades()
+        {
+            return View();
+        }
+    }
+}

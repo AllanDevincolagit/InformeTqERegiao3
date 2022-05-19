6590c7bcd221f51b75332f096ec73bbfa8485396
MaiiconDouglasd <maiicondouglasd@hotmail.com>
2022-05-19 11:57:40 -0300

    Conexão com banco de dados, falta adiconar editar, e deletar pra funcionar arrumar formulario de texto de envio problemas.

---
+++ InformeTqERegiao/InformeProblem.cs
@@ -0,0 +1,13 @@
+﻿namespace InformeTqERegiao
+{
+    public class InformeProblem
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Ocorrido { get; set; }
+    }
+}
--- InformeTqERegiao/Startup.cs
+++ InformeTqERegiao/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,7 @@ namespace InformeTqERegiao
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Contexto>(a => a.UseSqlServer("Server=(local);Database=IFTQ;Trusted_Connection=True;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
